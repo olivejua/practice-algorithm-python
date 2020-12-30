a4c720fe99220bc7887757467910dded9b4ecaf8
olivejua <tmfrl4710@gmail.com>
2020-12-30 23:55:59 +0900

    12/30 트리 연습

---
+++ 비선형 자료구조/트리/invert-binary-tree.py
@@ -0,0 +1,59 @@
+import collections
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree_s1(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = \
+                self.invertTree(root.right), self.invertTree(root.left)
+            return root
+        return None
+
+    # BFS
+    def invertTree_s2(self, root: TreeNode) -> TreeNode:
+        queue = collections.deque([root])
+
+        while queue:
+            node = queue.popleft()
+            # 부모 노드부터 하향식 스왑
+            if node:
+                node.left, node.right = node.right, node.left
+
+                queue.append(node.left)
+                queue.append(node.right)
+
+        return root
+
+    # 반복구조로 DFS
+    def invertTree_s3(self, root: TreeNode) -> TreeNode:
+        stack = collections.deque([root])
+
+        while stack:
+            node = stack.pop()
+            # 부모 노드부터 하향식 스왑
+            if node:
+                node.left, node.right = node.right, node.left
+
+                stack.append(node.left)
+                stack.append(node.right)
+
+        return root
+
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        stack = collections.deque([root])
+
+        while stack:
+            node = stack.pop()
+
+            if node:
+                stack.append(node.left)
+                stack.append(node.right)
+
+                node.left, node.right = node.right, node.left  # 후위 순회
+
+        return root
