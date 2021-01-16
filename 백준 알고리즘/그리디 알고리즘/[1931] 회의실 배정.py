54b7894813c98081409f9b58f9d561a7d7aee3a4
olivejua <tmfrl4710@gmail.com>
2021-01-16 19:11:35 +0900

    1/16 그리디 알고리즘 연습

--- 백준 알고리즘/그리디 알고리즘/[1931] 회의실 배정.py
+++ 백준 알고리즘/그리디 알고리즘/[1931] 회의실 배정.py
@@ -1,30 +1,34 @@
 import collections
-
-# n = int(input())
-# schedules = collections.defaultdict(list)
-# for _ in range(n):
-#     start, end = map(int, input().split())
-#     schedules[start].append(end)
-
-# max_cnt = 0
-# for key in sorted(schedules):
-#     cnt = 0
-#     start = key
-#     while start <= max(schedules.keys()):
-#         cnt += 1
-#
-#         # 끝나는 시간에 정확히 시작하는 시간이 없을 때 다음키가 존재할 때까지
-#         if start not in schedules:
-#             for i in range(start+1, max(schedules.keys())+1):
-#                 if i in schedules:
-#                     start = i
-#                     break
-#         start = schedules[start][0]
-#     max_cnt = max(max_cnt, cnt)
-#
-# print(max_cnt)
-
 import sys
+
+# 내가 푼 코드 
+
+n = int(input())
+schedules = collections.defaultdict(list)
+for _ in range(n):
+    start, end = map(int, input().split())
+    schedules[start].append(end)
+
+max_cnt = 0
+for key in sorted(schedules):
+    cnt = 0
+    start = key
+    while start <= max(schedules.keys()):
+        cnt += 1
+
+        # 끝나는 시간에 정확히 시작하는 시간이 없을 때 다음키가 존재할 때까지
+        if start not in schedules:
+            for i in range(start+1, max(schedules.keys())+1):
+                if i in schedules:
+                    start = i
+                    break
+        start = schedules[start][0]
+    max_cnt = max(max_cnt, cnt)
+
+print(max_cnt)
+
+
+# 참고한 코드
 
 input = sys.stdin.readline
 n = int(input())
