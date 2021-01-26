8c6ba73f8fae799083d8359e0cf613b56a5702b9
olivejua <tmfrl4710@gmail.com>
2021-01-26 23:18:27 +0900

    1/26 정렬 알고리즘 연습

---
+++ 프로그래머스/가장 큰 수.py
@@ -0,0 +1,16 @@
+def isSwitch(num1: int, num2: int) -> bool:
+    return str(num1)+str(num2) < str(num2)+str(num1)
+
+def solution(numbers):
+    i = 1
+    while i < len(numbers):
+        j = i
+        while 0 < j and isSwitch(numbers[j-1], numbers[j]):
+            numbers[j-1], numbers[j] = numbers[j], numbers[j-1]
+            j -= 1
+        i += 1
+
+    return ''.join(map(str, numbers))
+
+print(solution([6,10,2]))
+print(solution([3,30,34,5,9]))
