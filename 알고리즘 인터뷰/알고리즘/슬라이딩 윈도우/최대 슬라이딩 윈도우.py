b32369aedc52e0e23e34adc8671e93a9ca66e825
olivejua <tmfrl4710@gmail.com>
2021-01-29 23:40:07 +0900

    1/29 슬라이딩 윈도우 연습

---
+++ 알고리즘 인터뷰/알고리즘/슬라이딩 윈도우/최대 슬라이딩 윈도우.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    if not nums:
+        return nums
+
+    r = []
+    for i in range(len(nums) - k + 1):
+        r.append(max[nums[i:i + k]])
+
+    return r
