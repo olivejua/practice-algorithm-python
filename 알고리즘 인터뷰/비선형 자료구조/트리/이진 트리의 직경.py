52dcc24ca8c0a6e59e9ebeba46e23b570d25a501
olivejua <tmfrl4710@gmail.com>
2021-02-03 23:44:44 +0900

    2/3 이진트리 연습

--- 알고리즘 인터뷰/비선형 자료구조/트리/이진 트리의 직경.py
+++ 알고리즘 인터뷰/비선형 자료구조/트리/이진 트리의 직경.py
@@ -16,14 +16,11 @@ class Solution:
             if not node:
                 return -1
 
-            # 왼쪽, 오른쪽의 각 리프 노드까지 탐색
             left = dfs(node.left)
             right = dfs(node.right)
-            
-            # 가장 긴 경로
+
             self.longest = max(self.longest, left+right+2)
-            # 상태값
-            return max(left, right) + 1
+            return max(left, right)+1
 
         dfs(root)
         return self.longest
